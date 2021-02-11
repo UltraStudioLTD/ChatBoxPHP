d768f615314447903de29553c5219690373e2e45
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-02-11 22:23:53 +0400

    Update post.php

--- post.php
+++ post.php
@@ -6,6 +6,6 @@ if(isset($_SESSION['name'])){
         $text = $_POST['text'];
     };
     $text_message = "<div class='msgln'>⇒⇒ <span class='chat-time' style='color: gold'>".date("Y-m-d H:i:s")."</span> ➥ <b class='user-name' style='font-weight: bold; background: black; color: gold; padding: 2px 4px; font-size: 90%; border-radius: 4px; margin: 0 5px 0 0;'>".$_SESSION['name']."</b color='cyan'> ".stripslashes(htmlspecialchars($text))."<br></div>";
-    file_put_contents("log.html", base64_encode($text_message), FILE_APPEND | LOCK_EX);
+    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
