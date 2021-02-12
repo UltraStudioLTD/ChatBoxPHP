b636fade35d3bc29da2f9a1fbd9771de96e860a5
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-02-12 14:07:32 +0400

    Update index.php

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ function loginForm(){
                 <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/3.0/88x31.png" />
             </a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons Attribution-NonCommercial 3.0 Unported License</a>.
         </footer>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script type="text/javascript">
             // jQuery Document
             $(document).ready(function () {
